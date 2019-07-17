bfe17042d6b70438b3a0c6ea0b1cfa5cdaa61c12
Romańczuk Michał <romanczuk@prsa.pl>
2019-07-17 16:24:48 +0200

    Updates

---
+++ precache-manifest.627fd56d64f2fe6fbcd109d99828e40b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c9ff34a35bc984efcc4f2379d1c247",
+    "url": "/freshmail/index.html"
+  },
+  {
+    "revision": "776a78a14b68145ed5b0",
+    "url": "/freshmail/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "180220272d488e4a7520",
+    "url": "/freshmail/static/js/2.72175180.chunk.js"
+  },
+  {
+    "revision": "776a78a14b68145ed5b0",
+    "url": "/freshmail/static/js/main.acc720cb.chunk.js"
+  },
+  {
+    "revision": "fe640c7e2b4bdddc8835",
+    "url": "/freshmail/static/js/runtime~main.8ca01125.js"
+  }
+]);
